4f4ba7463342619197fde38baa9fb1a484645de9
DaniilKorol <97983323+DaniilKorol@users.noreply.github.com>
2024-03-11 23:32:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import requests, time, random, re
-import threading
-
-def generation():
-    response = requests.get(f'http://90.156.215.72:8234')
-
-time.sleep(5)
-threads = []
-for i in range(1600):
-    thread = threading.Thread(target=generation)
-    thread.start()
-    threads.append(thread)
-
-# Дождаться окончания всех потоков
-for thread in threads:
-    thread.join()
-
+ijnsiufns
